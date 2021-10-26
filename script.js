238c80075932f459a3fe39dfe551c2be54355441
unknown <git>
2021-10-26 11:52:47 +0530

    Updated1

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ let arr =[1,2,3,4,5];
 let res = arr.map((element)=>(element*3));
 console.log(res);
 
-********************** */
+//********************** */
 let student =[{
     name: 'alice',
     marks: 60},
@@ -26,7 +26,7 @@ let student =[{
 
 let res =student.filter((ele)=>marks>60);
     console.log(res.map((ele)=>marks.name));
-*********************** */
+//*********************** */
 let request = new XMLHttpRequest();
 request.open('GET','https://restcountries.com/v3.1/all',true);
 request.send();
@@ -35,15 +35,15 @@ request.onload=()=>{
     let res =data.filter((ele)=>ele.population<100000);
     console.log(res);
 }
-****************** */
+//****************** */
 let arr =['h','e','l','l','o']
 //let res= [...arr];
 console.log(...arr)
-******************** */
+//******************** */
 let arr = [1,2,3,4,5]
 let res =function(n1,n2,n3,n4,n5){
     console.log(n1+n2+n3+n4+n5);
 }
 res(...arr);
-------------------------
+//------------------------
 
